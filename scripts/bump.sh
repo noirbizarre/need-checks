118520442b99c98fa8f2ad58b7bc06400f5a56da
Axel H. <noirbizarre@gmail.com>
2024-02-26 17:15:26 +0100

    fix(release): consistent versioning and fixed hook scripts

--- scripts/bump.sh
+++ scripts/bump.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env sh
 
 # Replace latest tag by the bumped version in action.yml
+echo "Updating references in action and README (main ➡ ${CZ_PRE_NEW_VERSION})" >&2
 sed -i "s/main/${CZ_PRE_NEW_VERSION}/g" action.yml
+sed -i "s/main/${CZ_PRE_NEW_VERSION}/g" README.md
--- scripts/post-bump.sh
+++ scripts/post-bump.sh
@@ -4,20 +4,21 @@ VERSION=${CZ_POST_CURRENT_VERSION}
 
 # Create or update MAJOR and MAJOR.MINOR tags
 MAJOR_MINOR=${VERSION%.*}
-echo "Tagging <major>.<minor> version v${MAJOR_MINOR}"
-git tag -fa "v${MAJOR_MINOR}" -m "Update major.minor version tag"
-git push origin "v${MAJOR_MINOR}" --force
+echo "Tagging <major>.<minor> version ${MAJOR_MINOR}" >&2
+git tag -fa "${MAJOR_MINOR}" -m "Update major.minor version tag"
+git push origin "${MAJOR_MINOR}" --force
 
 MAJOR=${VERSION%%.*}
 if [ "${MAJOR}" != "0" ]; then
-    echo "Tagging <major> version v${MAJOR}"
-    git tag -fa "v${MAJOR}" -m "Update major version tag"
-    git push origin "v${MAJOR}" --force
+    echo "Tagging <major> version ${MAJOR}" >&2
+    git tag -fa "${MAJOR}" -m "Update major version tag"
+    git push origin "${MAJOR}" --force
 fi
 
 
 # Prepare next dev version
-echo "Prepare next development cycle"
+echo "Prepare next development cycle" >&2
 sed -i "s/${VERSION}/main/g" action.yml
+sed -i "s/${VERSION}/main/g" README.md
 git commit -am "build: prepare next \`main\` version"
 git push
